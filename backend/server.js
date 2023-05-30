7b436e09175ee264f1c6cf6c67b6ffd3811f7ba8
Jongjate Choomjairug <jongjate@isoftel.local>
2023-05-30 13:10:10 +0700

    feat:store into mysql

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,28 @@ import multer from 'multer';
 import cors from 'cors';
 import { v4 as uuidv4 } from 'uuid';
 import fs from 'fs';
+import dbConfig from './dbConfig.js';
+import mysql from 'mysql2'
+
+// Create a connection
+const connection = mysql.createConnection(dbConfig);
+
+// Connect to the MySQL server
+// connection.connect((err) => {
+//   if (err) {
+//     console.error('Error connecting to MySQL:', err);
+//     return;
+//   }
+//   console.log('Connected to MySQL server');
+// });
+
+// connection.end((err) => {
+//   if (err) {
+//     console.error('Error closing MySQL connection:', err);
+//     return;
+//   }
+//   console.log('MySQL connection closed');
+// });
 
 const app = express();
 
@@ -37,7 +59,7 @@ const storage = multer.diskStorage({
   }
 });
 
-let modifiedFileNameArray = []; 
+let modifiedFileNameArray = [];
 
 const upload = multer({ storage: storage, fileFilter: fileFilter });
 
@@ -63,9 +85,39 @@ app.post('/api/upload', (req, res, next) => {
         }
       })
 
-      console.log("Server-side files array: ", arrayFiles);
+
+      // Connect to the MySQL server
+      connection.connect((err) => {
+        if (err) {
+          console.error('Error connecting to MySQL:', err);
+          return;
+        }
+        console.log('Connected to MySQL server');
+
+        // const updatedFilesArray = ['filename1', null, 'filename3', null, null, 'filename6', null, 'filename8', 'filename9'];
+
+        const query = `
+    INSERT INTO applyforwork (fileAttach1, fileAttach2, fileAttach3, fileAttach4, fileAttach5, fileAttach6, fileAttach7, fileAttach8, fileAttach9)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+  `;
+
+        const dataArray = updatedFilesArray.map((file, index) => {
+          return file !== null ? file : '';
+        });
+
+        connection.query(query, dataArray, (error, results) => {
+          if (error) {
+            console.error('Error inserting data into MySQL:', error);
+            return;
+          }
+          console.log('Data inserted successfully');
+
+        });
+      });
+
+      // console.log("Server-side files array: ", arrayFiles);
       // console.log("ModifyName array: ", modifiedFileNameArray);
-      console.log('updateFile array: ', updatedFilesArray)
+      // console.log('updateFile array: ', updatedFilesArray)
       res.send('File upload complete!');
     }
   });
